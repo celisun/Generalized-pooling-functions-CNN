f5c0da86cf58d2fa805fbc0ce8b7b2dfacdbf7a2
celishxxp <sunceli@brandeis.edu>
2017-12-24 12:41:19 -0500

    Create image_flip_brightness.py

---
+++ image_processing/image_flip_brightness.py
@@ -0,0 +1,14 @@
+
+def image_flip_brightness(images, max_delta=63/255.0):
+    """
+    One technique of data augmentation
+    Ajust the brightness of image by a random factor picked in
+    a specified interval [-max_delta, max_delta],
+    this is done after flipping
+
+    """
+    images = tf.reshape(images, [-1, 32, 32, 3])
+    distorted_image = tf.map_fn(lambda img: tf.image.random_flip_left_right(img), images)
+    distorted_image = tf.map_fn(lambda img: tf.image.random_brightness(img, max_delta=max_delta), distorted_image)
+    distorted_image = tf.reshape(distorted_image, [-1, 32*32*3])
+    return distorted_image
