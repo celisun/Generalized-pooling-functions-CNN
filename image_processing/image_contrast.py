064595b68169667e0ec00eca6570826ecd4afda0
celishxxp <sunceli@brandeis.edu>
2017-12-24 12:41:04 -0500

    Create image_contrast.py

---
+++ image_processing/image_contrast.py
@@ -0,0 +1,12 @@
+
+def image_contrast(images, lower=0.2, upper=1.2):
+    """
+    One technique of data augmentation
+    Adjust the constrast of an image by a random factor,
+    picked in a specified interval [lower, upper],
+
+    """
+    images = tf.reshape(images, [-1, 32, 32, 3])
+    distorted_image = tf.map_fn(lambda img: tf.image.random_contrast(img, lower=lower, upper=upper), images)
+    distorted_image = tf.reshape(distorted_image, [-1, 32*32*3])
+    return distorted_image
