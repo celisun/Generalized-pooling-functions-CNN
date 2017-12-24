c6130129521714576deb899af8a537f5b09a4ed6
celishxxp <sunceli@brandeis.edu>
2017-12-24 12:40:23 -0500

    Create image_flipping.py

---
+++ image_processing/image_flipping.py
@@ -0,0 +1,12 @@
+
+def image_flipping(images):
+    """
+    One technique of data augmentation
+    randomly flip the image horizontally from left to right
+    """
+    images = tf.reshape(images, [-1, 32, 32, 3])
+    distorted_image = tf.map_fn(lambda img: tf.image.random_flip_left_right(img), images)
+    distorted_image = tf.reshape(distorted_image, [-1, 32*32*3])
+    return distorted_image
+
+
